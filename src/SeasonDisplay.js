884489db99bd67cda9800dae7c979889382ba04f
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-25 00:49:13 -0400

    add search config

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
+// Configuration for each season
+const searchConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun',
+  },
+  winter: {
+    text: 'Burr it is cold!',
+    iconName: 'snowflake',
+  },
+};
+
 // Get season
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -14,10 +26,15 @@ const getSeason = (lat, month) => {
 // Function Component
 const SeasonDisplay = ({ lat }) => {
   const season = getSeason(lat, new Date().getMonth());
-  const text =
-    season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+  const { text, iconName } = searchConfig[season];
 
-  return <h1>{text}</h1>;
+  return (
+    <div>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
