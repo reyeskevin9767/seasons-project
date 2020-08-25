2337dfc06acbc9f9cb1b72a56608737d02659e68
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-25 01:28:08 -0400

    fix cors issue

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
 // Configuration for each season
@@ -29,10 +30,10 @@ const SeasonDisplay = ({ lat }) => {
   const { text, iconName } = searchConfig[season];
 
   return (
-    <div>
-      <i className={`${iconName} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   );
 };
